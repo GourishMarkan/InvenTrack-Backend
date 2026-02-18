9ca28cc2b1a29126b382887be591d4e5f0a8616e
gourishmarkan <markangourish9@gmail.com>
2026-02-18 10:16:56 +0530

    refactor: remove constructor from PrismaService for cleaner initialization

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,17 +3,17 @@ import { PrismaClient } from 'src/generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit{
-    constructor(){
-        super(
-            {
-                datasources:{
-                    db:{
-                        url:process.env.DATABASE_URL
-                    }
-                }
-            }
-        )
-    }
+    // constructor(){
+    //     super(
+    //         {
+    //             datasources:{
+    //                 db:{
+    //                     url:process.env.DATABASE_URL
+    //                 }
+    //             }
+    //         }
+    //     )
+    // }
   async onModuleInit() {
       await this.$connect();
   }
